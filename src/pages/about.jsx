48a914f5d30e6666eac502202e1c86a036e844a2
iluna007 <146373472+iluna007@users.noreply.github.com>
2024-10-26 16:18:30 +0100

    nuevos cambios

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -29,27 +29,27 @@ const About = () => {
               <p>
                 Finnja Willner is a filmmaker, visual anthropologist,
                 transdisciplinary artist and somatic movement educator with
-                focus on dance.
+                focus on dance. In her projects she interweaves the different
+                disciplines she is moving in, creating a strong, poetic and
+                sensitive visual language in film and photography. She captures
+                different life realities with honesty and care and aims to build
+                bridges through her artwork.
               </p>
               <p>
-                In her projects she often interweaves the different disciplines
-                she is moving in. She has a strong, poetic and sensitive visual
-                language, both in film and photography, through which she aims
-                to make different realities of life tangible. Sometimes she
-                works as a one-woman-crew and is experienced in camera,
-                directing, editing and production. But mainly she very much
-                enjoys working in a team.
+                At times, she works as a one-woman-crew, skilled in camera work,
+                directing, editing and production. However, she also enjoys the
+                collaborative process, bringing her unique perspective to a team
+                and contributing to the collective vision.
               </p>
               <p>
-                Next to her artistic work she produces audiovisual web and
-                exhibition content for different clients and has developed and
-                facilitated a broad variety of workshops in an international
-                context which she teaches with a lot of passion. She works
-                aligned with her values, has worked in political education for
-                many years and is a certified Social Justice and Diversity
-                trainer.
+                Next to her artistic work she creates audiovisual content for
+                web and exhibitions for diverse clients. She has designed and
+                facilitated a broad variety of workshops in international
+                contexts which she teaches with passion. She works aligned with
+                her values, has worked in political education for many years and
+                is a certified Social Justice and Diversity trainer.
               </p>
-
+              <div className="mt-5"></div>
               <p>
                 <strong>FILM AND SOMATICS</strong> For me, images are embodied
                 and lived experiences. Profoundly poetic images are
@@ -85,21 +85,20 @@ const About = () => {
               <hr />
               <h2 className="mt-5">Teaching/Workshop Facilitator:</h2>
               <p>
-                Spots create opportunities for democratic exchange with
-                workshops on film education and anti-discrimination (workshop
-                facilitator in 2023 & 2024).
                 <a href=" https://spots.deutsche-filmakademie.de/externe/wl-finnja-willner/">
                   {" "}
-                  https://spots.deutsche-filmakademie.de/externe/wl-finnja-willner/
+                  Spots
                 </a>
+                create opportunities for democratic exchange with workshops on
+                film education and anti-discrimination (workshop facilitator in
+                2023 & 2024).
               </p>
               <p>
-                Deutsche Kinemathek offers a variety of film workshops
-                (facilitator for Stop Motion, Meet Berlin Filmmakers and Smart
-                Film Safari since 2017).
                 <a href="https://www.deutsche-kinemathek.de/de/besuch/bildung-vermittlung">
-                  https://www.deutsche-kinemathek.de/de/besuch/bildung-vermittlung
+                  Deutsche Kinemathek
                 </a>
+                offers a variety of film workshops (facilitator for Stop Motion,
+                Meet Berlin Filmmakers and Smart Film Safari since 2017).
               </p>
               <p>
                 Escuela Manolo Blahnik La Palma (workshop facilitator for
@@ -107,46 +106,42 @@ const About = () => {
                 Islands in 2022).
               </p>
               <p>
-                filmArche Summer School brings filmmakers from all over the
-                world together to exchange and co-create at filmArche Berlin
-                (organizer & teacher in 2018 & 2019).
                 <a href="https://utopiafilmarche.wordpress.com/about-us/">
-                  https://utopiafilmarche.wordpress.com/about-us/
+                  filmArche
                 </a>
+                Summer School brings filmmakers from all over the world together
+                to exchange and co-create at filmArche Berlin (organizer &
+                teacher in 2018 & 2019).
               </p>
               <p>
-                Monnem Movies (project initiator & workshop facilitator in
-                2018).
                 <a
                   href="                https://www.mannheimer-morgen.de/orte/mannheim_artikel,-mannheim-stadtteile-aus-buergersicht-als-film-_arid,1254818.html?&npg
 "
                 >
                   {" "}
-                  https://www.mannheimer-morgen.de/orte/mannheim_artikel,-mannheim-stadtteile-aus-buergersicht-als-film-_arid,1254818.html?&npg
+                  Monnem Movies
                 </a>
+                (project initiator & workshop facilitator in 2018).
               </p>
               <p>
-                EmpoderArte Peru empowers women through filmmaking in Trujillo,
-                Peru (workshop facilitator in 2018).
                 <a href="https://grenzenlos-verein.jimdofree.com/unsere-projekte/empoderarte-frauen-machen-filme/">
-                  https://grenzenlos-verein.jimdofree.com/unsere-projekte/empoderarte-frauen-machen-filme/
-                </a>
+                  EmpoderArte
+                </a>{" "}
+                Peru empowers women through filmmaking in Trujillo, Peru
+                (workshop facilitator in 2018).
               </p>
               <p>
-                Cinetaya (co-founder & workshop facilitator in 2015 & 2016) is a
-                film workshop initiative that offers documentary film workshops
-                in an intercultural context. The first workshops took place at
-                the cultural centre Casa Tres Mundos in Granada, Nicaragua.
-                <a href="https://c3mundos.org/de/cine-documental/">
-                  https://c3mundos.org/de/cine-documental/
-                </a>
+                <a href="https://c3mundos.org/de/cine-documental/">Cinetaya</a>{" "}
+                (co-founder & workshop facilitator in 2015 & 2016) is a film
+                workshop initiative that offers documentary film workshops in an
+                intercultural context. The first workshops took place at the
+                cultural centre Casa Tres Mundos in Granada, Nicaragua.
               </p>
               <p>
-                Sensory Lab is a digital experiment to connect through senses &
-                movement.
                 <a href="https://www.creative-city-berlin.de/en/events/event/reflection-of-a-dancing-body-with-finnja-willner/">
-                  https://www.creative-city-berlin.de/en/events/event/reflection-of-a-dancing-body-with-finnja-willner/
+                  Sensory Lab
                 </a>
+                is a digital experiment to connect through senses & movement.
               </p>
             </div>
 
@@ -156,7 +151,7 @@ const About = () => {
             <div className="col-2"></div>
             <div className="col">
               <hr />
-              <h2 className="mt-5">Clients</h2>
+              <h2 className="mt-5">Clients / Collaborators</h2>
               <p>xxx</p>
             </div>
 
