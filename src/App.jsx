5225b5e12ed6eb764020832c93c1650e445ceab1
iluna007 <146373472+iluna007@users.noreply.github.com>
2024-11-09 17:02:49 +0000

    add top buttom

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 import "./App.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
-import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
-import { Routes, Route } from "react-router-dom"; // Import Routes and Route
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/home";
@@ -11,29 +11,27 @@ import About from "./pages/about";
 import Contact from "./pages/contact";
 import Projects from "./pages/projects";
 import ProjectDetail from "./components/ProjectDetail";
+import ScrollToTopButton from "./components/ScrollToTopButton"; // Import ScrollToTopButton
 
 function App() {
-  // Add state for the artist
   const [artist, setArtist] = useState("Visual Artist");
 
   return (
     <>
       <BrowserRouter>
-        {/* Pass the artist state to Navbar */}
         <Navbar artist={artist} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route
             path="/projects"
-            // Pass setArtist function to Projects so it can update the artist name
             element={<Projects setArtist={setArtist} />}
           />
-          <Route path="/projects/:id" element={<ProjectDetail />} />{" "}
-          {/* Route for project details */}
+          <Route path="/projects/:id" element={<ProjectDetail />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
         <Footer />
+        <ScrollToTopButton /> {/* Add ScrollToTopButton here */}
       </BrowserRouter>
     </>
   );
